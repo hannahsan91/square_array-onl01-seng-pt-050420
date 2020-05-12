60c1d271f274f420a12c30b5720e54c738582263
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-12 00:09:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,8 @@
 def square_array(array)
-  array = [1, 2, 3]
+  squared_array []
+  array.each do |num|
+    square = num ** 2
+    squared_array << square
 end
-square_array(array)
+  squared_array
+end
